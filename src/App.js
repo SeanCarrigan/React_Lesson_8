2b672d687b3265983a4cd494bd9b451370cbabf0
SeanCarrigan <112054424+SeanCarrigan@users.noreply.github.com>
2024-01-31 13:51:29 -0600

    React lesson 8: Props & Prop Drilling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
+import { useState } from "react";
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
+import AddItem from "./AddItem";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Item 1",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    // const listItems = items.map((item) =>
+    //   item.id === id ? { ...item, checked: !item.checked } : item
+    // );
+
+    // same logic as above - the forEach is just easier for me to read
+    const listItems = [];
+    items.forEach((item) => {
+      if (item.id === id) {
+        item.checked = !item.checked;
+      }
+      listItems.push(item);
+    });
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+  const handleDelete = (id) => {
+    // const listItems = items.filter((item) => item.id !== id);
+    // same logic as above - the forEach is just easier for me to read
+    const listItems = [];
+    items.forEach((item) => {
+      if (item.id !== id) {
+        listItems.push(item);
+      }
+    });
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Header title="Grocery List" />
+      <AddItem />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      ></Content>
+      <Footer length={items.length} />
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,16 @@
-const Footer = () => {
+const Footer = ({ length }) => {
   const today = new Date();
+
+  // const displayListCount = () => {
+  //   return length > 1 ? "List Items" : "List Item";
+  // };
 
   return (
     <footer>
+      <p>
+        {/* {length} {displayListCount()} */}
+        {length} List {length === 1 ? "item" : "items"}
+      </p>
       <p>Copyright &copy; {today.getFullYear()} </p>
     </footer>
   );
